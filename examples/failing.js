d4968ac675ebbd008c6a80c7e5d7323d951b5235
espen dalløkken <espen.dallokken@finn.no>
2024-10-30 14:39:34 +0100

    adding examples to enabled more collaboration

---
+++ examples/failing.js
@@ -0,0 +1,29 @@
+import HttpClient, { HttpClientError } from '../lib/http-client.js';
+
+/**
+ * Example of the circuit breaker opening on a failing request.
+ */
+const client = new HttpClient();
+try {
+    await client.request({
+        origin: 'http://localhost:9099',
+        path: '/',
+        method: 'GET',
+    });
+} catch (_) {
+    // Mute the first one, next request should hit an open breaker
+}
+
+try {
+    await client.request({
+        origin: 'http://localhost:9099',
+        path: '/',
+        method: 'GET',
+    });
+} catch (err) {
+    if (err instanceof HttpClientError) {
+        if (err.code === HttpClientError.ServerDown) {
+            console.error('Server unavailable..');
+        }
+    }
+}
